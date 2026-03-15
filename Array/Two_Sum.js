39301c4799678360c977e799930bbef82f55da48
Manisha Singh <manishasinghbcs89@gmail.com>
2026-03-15 23:56:10 +0530

    Worked on Array Two Sum.

---
+++ Array/Two_Sum.js
@@ -0,0 +1,11 @@
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+let result = twoSum([2, 4, 6, 7], 10);
+console.log(result);
